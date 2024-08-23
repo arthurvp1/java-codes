3537f1c793a704489d88d00f87a369def9e4ad6c
arthurvp1 <arthurviecilipinheirodasilva@gmail.com>
2024-08-23 10:02:47 -0300

    nova atividade

--- java-code/firstCode.java
+++ java-code/firstCode.java
@@ -5,34 +5,32 @@ public class firstCode {
         int tempo, vm, distancia, litros;
         char veiculo;
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Você veio de onibus ou carro (o/c)");
-        veiculo = scanner.next().charAt(0);
-
-        if(veiculo == 'c'){
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Você veio de onibus ou carro (o/c)");
+            veiculo = scanner.next().charAt(0);
             
-            
-            
-            System.out.println("Digite o tempo gasto na viagem (h): ");
-            tempo = scanner.nextInt();
-            
-            System.out.println("Agora digite a velocidade média da viagem: (km)");
-            vm = scanner.nextInt();
-            
-            distancia = (tempo*vm);
-            
-            System.out.println ("A distancia é: " + distancia+"km");
-            
-            litros = (int) (distancia/12);
-            
-            System.out.println("Você gastou nessa viagem, "+litros+" de gasolina");
-        }else{
-
-            System.out.println("Você nao gastou nada, seu safado!");
+            if(veiculo == 'c'){
+                
+                
+                
+                System.out.println("Digite o tempo gasto na viagem (h): ");
+                tempo = scanner.nextInt();
+                
+                System.out.println("Agora digite a velocidade média da viagem: (km)");
+                vm = scanner.nextInt();
+                
+                distancia = (tempo*vm);
+                
+                System.out.println ("A distancia é: " + distancia+"km");
+                
+                litros = (int) (distancia/12);
+                
+                System.out.println("Você gastou nessa viagem, "+litros+"L de gasolina");
+            }else{
+                
+                System.out.println("Você nao gastou nada, seu safado!");
+            }
         }
-              
-            scanner.close();
         }
         
     }
